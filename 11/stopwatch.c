362908839716ddfc18aab3abbe2ebf29e5fe76a1
hakong97_mac <hark_isd@naver.com>
2022-05-17 16:16:06 +0900

    SIGTERM으로 변경, 주석 추가

--- 11/stopwatch.c
+++ 11/stopwatch.c
@@ -7,18 +7,28 @@ int i = 0;
 void sig_alarm(int signo)
 {
 	i++;
-	printf("Stopwatch : %d\n", i);
+	printf("Stopwatch : %d\n", i); //초 출력
+}
+void sig_int(int signo)
+{
+	printf("Stop : %d\n", i); //중단했을 때 초 출력
+	exit(1); //프로그램 종료
 }
 int main (void)
 {
 	if(signal(SIGALRM, sig_alarm) == SIG_ERR)
 	{
-		perror("SIGNAL");
+		perror("SIGNAL_ALRM");
+		exit(1);
+	}
+	if(signal(SIGINT, sig_int) == SIG_ERR) // Ctrl+C를 통해 스탑워치 중단
+	{
+		perror("SIGNAL_INT");
 		exit(1);
 	}
 	while(1)
 	{
-		alarm(1);
-		pause();
+		alarm(1); // 1초 간격으로 alarm()
+		pause(); 
 	}
 }
--- 11/tlimit.c
+++ 11/tlimit.c
@@ -8,7 +8,9 @@
 void sig_alarm(int signo)
 {
 	printf("TIME LIMIT! PROCESS END\n");
-	kill(0, SIGINT);
+	kill(0, SIGTERM); //SIGTERM 시그널 전송
+	while(wait(NULL) != -1); //Child Process가 종료될 때 까지 대기
+	exit(1); // 프로그램 종료
 }
 int main (int argc, char* argv[])
 {
@@ -19,25 +21,25 @@ int main (int argc, char* argv[])
 		exit(1);
 	}
 	
-	if((pid = fork()) == 0) //child
+	if((pid = fork()) == 0) //Child Process 생성
 	{
 		char command[BUFSIZ];
-		sprintf(command, "./%s",argv[2]);
-		if(execl("/bin/sh", "sh", "-c", command, (char*)NULL) == -1)
+		sprintf(command, "./%s",argv[2]); //Child에서 실행할 프로그램 명령줄 생성
+		if(execl("/bin/sh", "sh", "-c", command, (char*)NULL) == -1) //프로그램 실행
 		{
 			perror("execl");
 			exit(1);
 		}
 	}
-	else
+	else //Parent
 	{
-		if(signal(SIGALRM, sig_alarm) == SIG_ERR)
+		if(signal(SIGALRM, sig_alarm) == SIG_ERR) //SIGALRM에 대한 핸들러 지정
 		{
 			perror("SIGNAL");
 			exit(1);
 		}
-		alarm(atoi(argv[1]));
-		while(wait(NULL) != -1);
+		alarm(atoi(argv[1])); //alarm 설정
+		pause();
 	}
 
 }

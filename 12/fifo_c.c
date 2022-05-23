b624a49f309ceeeb4b571cfe58041f78c20d152f
hakong97 <hark_isd@naver.com>
2022-05-23 22:25:39 +0900

    add comment

--- 12/fifo_c.c
+++ 12/fifo_c.c
@@ -15,13 +15,13 @@ int main (void)
 	int pd_send, pd_receive;
 	pthread_t tid1, tid2;
 
-	printf(" ----- FULL DUPLEX COMMUNICATION (CLIENT) -----\n");
-	if(mkfifo("./CtoS_FIFO", 0666) == -1)
+	printf(" ----- (CLIENT) -----\n");
+	if(mkfifo("./CtoS_FIFO", 0666) == -1) //CtoS_FIFO 생성
 	{
 		perror("mkfifo");
 		exit(1);
 	}
-	if((pd_receive = open("./StoC_FIFO", O_RDONLY|O_NONBLOCK)) == -1)
+	if((pd_receive = open("./StoC_FIFO", O_RDONLY|O_NONBLOCK)) == -1) // read에서 block되는걸 방지하기 위해 nonblock으로 생성
 	{
 		perror("open");
 		exit(1);
@@ -32,10 +32,10 @@ int main (void)
 		perror("open");
 		exit(1);
 	}
-
+	/* 쓰레드를 통해 구현하였음. */
 	pthread_create(&tid1, NULL, t_send, &pd_send);
 	pthread_create(&tid2, NULL, t_receive, &pd_receive);
-	while(1);
+	while(1); 
 
 	
 	close(pd_send);
@@ -46,18 +46,18 @@ void* t_send(void* data)
 {
 	int pd, n;
 	char msg_send[64];
-	pd = *((int*)data);
+	pd = *((int*)data); //
 	while(1)
 	{
 		write(1, "To Server : ", 12);
-		read(0, msg_send, 64);
-		n = write(pd, msg_send, strlen(msg_send)+1);
+		read(0, msg_send, 64); //보낼 메세지 입력받는다
+		n = write(pd, msg_send, strlen(msg_send)+1); //파이프를 통해 전송
 		if(n==-1)
 		{
 			perror("write");
 			exit(1);
 		}
-		memset(msg_send, 0, 64);
+		memset(msg_send, 0, 64); //버퍼를 지워준다. (엔터입력 시 재전송되는 경우 방지)
 	}
 }
 
@@ -68,10 +68,10 @@ void* t_receive(void* data)
 	pd = *((int*)data);
 	while(1)
 	{
-		if((n=read(pd, msg_receive, 64)) > 0)
+		if((n=read(pd, msg_receive, 64)) > 0) //받은 메세지를 읽는다.
 		{
 			write(1, "\nFrom Server : ", 15);
-			write(1, msg_receive, n);
+			write(1, msg_receive, n); //받은 메세지 출력
 			write(1, "To Server : ", 13);
 			continue;
 		}
--- 12/fifo_s.c
+++ 12/fifo_s.c
@@ -15,8 +15,8 @@ int main (void)
 	int pd_send, pd_receive;
 	pthread_t tid1, tid2;
 
-	printf("----- FULL DUPLEX COMMUNICATION (SERVER) -----\n");
-	if(mkfifo("./StoC_FIFO", 0666) == -1)
+	printf("----- (SERVER) -----\n");
+	if(mkfifo("./StoC_FIFO", 0666) == -1) //StoC_FIFO 생성
 	{
 		perror("mkfifo");
 		exit(1);
@@ -27,18 +27,13 @@ int main (void)
 		exit(1);
 	}
 
-	if((pd_receive = open("./CtoS_FIFO", O_RDONLY|O_NONBLOCK)) == -1)
+	if((pd_receive = open("./CtoS_FIFO", O_RDONLY|O_NONBLOCK)) == -1) //read시 block 되는것을 방지
 	{
 		perror("open");
 		exit(1);
 	}
 
-	if(signal(SIGINT, sig_int) == SIG_ERR)
-	{
-		perror("SIGNAL");
-		exit(1);
-	}
-
+	/*쓰레드를 통해 구현하였음.*/
 	pthread_create(&tid1, NULL, t_send, &pd_send);
 	pthread_create(&tid2, NULL, t_receive, &pd_receive);
 	
@@ -53,14 +48,14 @@ void* t_send(void* data)
 	while(1)
 	{
 		write(1, "To Client : ", 12);
-		read(0, msg_send, 64);
-		n = write(pd, msg_send, strlen(msg_send)+1);
+		read(0, msg_send, 64); //보낼 메세지를 읽는다.
+		n = write(pd, msg_send, strlen(msg_send)+1); //메세지를 전송
 		if(n == -1)
 		{
 			perror("write");
 			exit(1);
 		}
-		memset(msg_send, 0, 64);
+		memset(msg_send, 0, 64); //msg_send 버퍼를 비운다.
 	}
 }
 
@@ -71,10 +66,10 @@ void* t_receive(void* data)
 	pd = *((int*)data);
 	while(1)
 	{
-		if((n=read(pd, msg_receive, 64)) > 0)
+		if((n=read(pd, msg_receive, 64)) > 0) //받은 메세지를 읽는다.
 		{
 			write(1, "\nFrom Client : ",15);
-			write(1, msg_receive, n);
+			write(1, msg_receive, n); //받은 메세지 출력
 			write(1, "To Client : ", 13);
 			continue;
 		}

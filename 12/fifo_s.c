9d52a85c77e652a36dfda178d376c909f4ff60a5
hakong97 <hark_isd@naver.com>
2022-05-23 22:19:18 +0900

    full duplex communication final

--- 12/fifo_s.c
+++ 12/fifo_s.c
@@ -4,12 +4,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
+#include <signal.h>
+
+void* t_send(void* data);
+void* t_receive(void* data);
 
 int main (void)
 {
-	int pd_send, pd_receive, n;
-	char msg_send[64];
-	char msg_receive[64];
+	int pd_send, pd_receive;
+	pthread_t tid1, tid2;
 
 	printf("----- FULL DUPLEX COMMUNICATION (SERVER) -----\n");
 	if(mkfifo("./StoC_FIFO", 0666) == -1)
@@ -29,26 +33,50 @@ int main (void)
 		exit(1);
 	}
 
+	if(signal(SIGINT, sig_int) == SIG_ERR)
+	{
+		perror("SIGNAL");
+		exit(1);
+	}
+
+	pthread_create(&tid1, NULL, t_send, &pd_send);
+	pthread_create(&tid2, NULL, t_receive, &pd_receive);
+	
+	while(1);
+}
+
+void* t_send(void* data)
+{
+	int pd, n;
+	char msg_send[64];
+	pd = *((int*)data);
 	while(1)
 	{
-		if((n=read(pd_receive, msg_receive, 64)) > 0)
+		write(1, "To Client : ", 12);
+		read(0, msg_send, 64);
+		n = write(pd, msg_send, strlen(msg_send)+1);
+		if(n == -1)
 		{
-			printf("\nFrom Client : ");
+			perror("write");
+			exit(1);
+		}
+		memset(msg_send, 0, 64);
+	}
+}
+
+void* t_receive(void* data)
+{
+	int pd, n;
+	char msg_receive[64];
+	pd = *((int*)data);
+	while(1)
+	{
+		if((n=read(pd, msg_receive, 64)) > 0)
+		{
+			write(1, "\nFrom Client : ",15);
 			write(1, msg_receive, n);
+			write(1, "To Client : ", 13);
 			continue;
 		}
-		else
-		{
-			printf("\nTo Client : ");
-			scanf("%s", msg_send);
-			n = write(pd_send, msg_send, strlen(msg_send)+1);
-			if(n == -1)
-			{
-				perror("write");
-				exit(1);
-			}
-		}
 	}
-	close(pd_send);
-	close(pd_receive);
 }
